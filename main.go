6c245466400873dba40d1eff36a8b546a734ffa2
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-23 15:50:11 +0300

    Revert "test"
    This reverts commit 9c9ffae021713933699d87bc198dfd8f8444ab02.

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// +build go1.9
+
 package main
 
 import (
@@ -40,14 +42,6 @@ func startPayoutsProcessor() {
 	u.Start()
 }
 
-
-func startExchangeProcessor() {
-	u := exchange.StartExchangeProcessor(&cfg.Exchange, backend)
-	u.Start()
-}
-
-
-
 func startNewrelic() {
 	if cfg.NewrelicEnabled {
 		nr := gorelic.NewAgent()
@@ -88,11 +82,10 @@ func main() {
 
 	startNewrelic()
 
-	backend = storage.NewRedisClient(&cfg.Redis, cfg.Coin, cfg.Pplns, cfg.CoinName)
+	backend = storage.NewRedisClient(&cfg.Redis, cfg.Coin, cfg.Pplns)
 	pong, err := backend.Check()
 	if err != nil {
 		log.Printf("Can't establish connection to backend: %v", err)
-                //os.Exit(0)
 	} else {
 		log.Printf("Backend check reply: %v", pong)
 	}
@@ -109,11 +102,6 @@ func main() {
 	if cfg.Payouts.Enabled {
 		go startPayoutsProcessor()
 	}
-
-	if cfg.Exchange.Enabled {
-		go startExchangeProcessor()
-	}
-
 	quit := make(chan bool)
 	<-quit
 }
