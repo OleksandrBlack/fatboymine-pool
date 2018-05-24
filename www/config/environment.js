717d9689a3a73a0caa031fc5beb4cbbfd173dd63
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-24 17:54:10 +0300

    Revert "test"
    This reverts commit 5dcabfe24730a87bd9be7b8b05553e32f324b500.

--- www/config/environment.js
+++ www/config/environment.js
@@ -20,8 +20,6 @@ module.exports = function(environment) {
       PPLNS: '3000',
       blocksMature: '120',
       minPayouts: '0.05',
-	  
-	  defaultLocale: 'en',
 		
       // API host and port
       ApiUrl: '//eth.fatboymine.com/',
