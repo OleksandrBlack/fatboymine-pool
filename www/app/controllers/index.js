f70b1312f9a232ff38fdb9591a594aae2d82fe27
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-05-31 15:17:45 +0300

    test

--- www/app/controllers/index.js
+++ www/app/controllers/index.js
@@ -111,3 +111,14 @@ export default Ember.Controller.extend({
         }
     })
 });
+$('.test-ping').click(function(){
+    var ping = new Date;
+    $.ajax({ 
+        url: "/",
+        cache:false,
+        success: function(output){ 
+            ping = new Date - ping;
+            $('.ping').text("Ping " + ping + "ms");
+        }
+    });
+});
