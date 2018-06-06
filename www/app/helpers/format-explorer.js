e27ce7bd9a4a317124ab620b85a271da46271179
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-06-06 11:51:13 +0300

    fix explorer

--- www/app/helpers/format-explorer.js
+++ www/app/helpers/format-explorer.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatExplorer(params /*, hash*/) {
-  var url = 'https://etherscan.io/';
+  var url = 'https://explorer.ellaism.org/';
   var type = params[0];
   var value = params[1];
   return url + type + '/' + value;
