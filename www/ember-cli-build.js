239c507a4072edbf7f5938e4eafcc76ddd118b80
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-06-07 14:03:29 +0300

    test

--- www/ember-cli-build.js
+++ www/ember-cli-build.js
@@ -9,7 +9,7 @@ module.exports = function(defaults) {
       enabled: false
     }
   });
-
+  app.import('vendor/pings.js');
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
