7caa7c8da03c81ff1b9e07ab697fb0c31ef233e3
Lucky-Mining <aleksandr.chornuy@gmail.com>
2018-06-07 15:29:49 +0300

    test

--- www/app/helpers/format-ping.js
+++ www/app/helpers/format-ping.js
@@ -1,37 +1,24 @@
 import Ember from 'ember';
-
 var p = new Ping();
-var ua = "https://ella.fatboymine.com";
-var ru = "https://esn.solopool.org";
-var selector_ua = "ping-ua";
-var selector_ru = "ping-ru";
+var site = "https://fatboymine.com";
+var selector = "ping-pool";
 
-export function startPingUa(address, selector_ua) {
+export function startPing(address, selector) {
   p.ping(address, function(err, data) {
     if (err) {
       data = data + " " + err;
     }
     
-    document.getElementById(selector_ua).innerHTML = data;
-  });
-}
-export function startPingRu(address, selector_ru) {
-  p.ping(address, function(err, data) {
-    if (err) {
-      data = data + " " + err;
-    }
-    
-    document.getElementById(selector_ru).innerHTML = data;
+    document.getElementById(selector).innerHTML = data;
   });
 }
 
-startPingUa(ua, selector_ua);
-startPingRu(ru, selector_ru);
 
-setInterval(function() {
-	startPingUa(ua, selector_ua);
-	startPingRu(ru, selector_ru);
-}, 10000);
+export function startPingHelper() {
+  var address = arguments[0];
+  var selector = arguments[1];
+  
+	return startPing(address, selector);
+}
 
-export default Ember.Helper.helper(startPingUa);
-export default Ember.Helper.helper(startPingRu);
+export default Ember.Helper.helper(startPingHelper);
